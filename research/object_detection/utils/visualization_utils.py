5cc2380b78ef309f344d5add6b17692b31f06e5a
umang-aditya <53267493+umang-aditya@users.noreply.github.com>
2020-03-29 08:39:26 +0530

    Add files via upload

--- research/object_detection/utils/visualization_utils.py
+++ research/object_detection/utils/visualization_utils.py
@@ -536,7 +536,7 @@ def draw_side_by_side_evaluation_image(eval_dict,
   # Add the batch dimension if the eval_dict is for single example.
   if len(eval_dict[detection_fields.detection_classes].shape) == 1:
     for key in eval_dict:
-      if key != input_data_fields.original_image and key != input_data_fields.image_additional_channels:
+      if key != input_data_fields.original_image:
         eval_dict[key] = tf.expand_dims(eval_dict[key], 0)
 
   for indx in range(eval_dict[input_data_fields.original_image].shape[0]):
@@ -600,42 +600,8 @@ def draw_side_by_side_evaluation_image(eval_dict,
         max_boxes_to_draw=None,
         min_score_thresh=0.0,
         use_normalized_coordinates=use_normalized_coordinates)
-    images_to_visualize = tf.concat([images_with_detections,
-                                     images_with_groundtruth], axis=2)
-
-    if input_data_fields.image_additional_channels in eval_dict:
-      images_with_additional_channels_groundtruth = (
-          draw_bounding_boxes_on_image_tensors(
-              tf.expand_dims(
-                  eval_dict[input_data_fields.image_additional_channels][indx],
-                  axis=0),
-              tf.expand_dims(
-                  eval_dict[input_data_fields.groundtruth_boxes][indx], axis=0),
-              tf.expand_dims(
-                  eval_dict[input_data_fields.groundtruth_classes][indx],
-                  axis=0),
-              tf.expand_dims(
-                  tf.ones_like(
-                      eval_dict[input_data_fields.groundtruth_classes][indx],
-                      dtype=tf.float32),
-                  axis=0),
-              category_index,
-              original_image_spatial_shape=tf.expand_dims(
-                  eval_dict[input_data_fields.original_image_spatial_shape]
-                  [indx],
-                  axis=0),
-              true_image_shape=tf.expand_dims(
-                  eval_dict[input_data_fields.true_image_shape][indx], axis=0),
-              instance_masks=groundtruth_instance_masks,
-              keypoints=None,
-              max_boxes_to_draw=None,
-              min_score_thresh=0.0,
-              use_normalized_coordinates=use_normalized_coordinates))
-      images_to_visualize = tf.concat(
-          [images_to_visualize, images_with_additional_channels_groundtruth],
-          axis=2)
-    images_with_detections_list.append(images_to_visualize)
-
+    images_with_detections_list.append(
+        tf.concat([images_with_detections, images_with_groundtruth], axis=2))
   return images_with_detections_list
 
 
@@ -1073,6 +1039,80 @@ class EvalMetricOpsVisualization(six.with_metaclass(abc.ABCMeta, object)):
     """
     raise NotImplementedError
 
+def return_coordinates(
+    image,
+    boxes,
+    classes,
+    scores,
+    category_index,
+    instance_masks=None,
+    instance_boundaries=None,
+    keypoints=None,
+    use_normalized_coordinates=False,
+    max_boxes_to_draw=20,
+    min_score_thresh=.5,
+    agnostic_mode=False,
+    line_thickness=4,
+    groundtruth_box_visualization_color='black',
+    skip_scores=False,
+    skip_labels=False):
+  # Create a display string (and color) for every box location, group any boxes
+  # that correspond to the same location.
+  box_to_display_str_map = collections.defaultdict(list)
+  box_to_color_map = collections.defaultdict(str)
+  box_to_instance_masks_map = {}
+  box_to_instance_boundaries_map = {}
+  box_to_score_map = {}
+  box_to_keypoints_map = collections.defaultdict(list)
+  if not max_boxes_to_draw:
+    max_boxes_to_draw = boxes.shape[0]
+  for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+    if scores is None or scores[i] > min_score_thresh:
+      box = tuple(boxes[i].tolist())
+      if instance_masks is not None:
+        box_to_instance_masks_map[box] = instance_masks[i]
+      if instance_boundaries is not None:
+        box_to_instance_boundaries_map[box] = instance_boundaries[i]
+      if keypoints is not None:
+        box_to_keypoints_map[box].extend(keypoints[i])
+      if scores is None:
+        box_to_color_map[box] = groundtruth_box_visualization_color
+      else:
+        display_str = ''
+        if not skip_labels:
+          if not agnostic_mode:
+            if classes[i] in category_index.keys():
+              class_name = category_index[classes[i]]['name']
+            else:
+              class_name = 'N/A'
+            display_str = str(class_name)
+        if not skip_scores:
+          if not display_str:
+            display_str = '{}%'.format(int(100*scores[i]))
+          else:
+            display_str = '{}: {}%'.format(display_str, int(100*scores[i]))
+        box_to_display_str_map[box].append(display_str)
+        box_to_score_map[box] = scores[i]
+        if agnostic_mode:
+          box_to_color_map[box] = 'DarkOrange'
+        else:
+          box_to_color_map[box] = STANDARD_COLORS[
+              classes[i] % len(STANDARD_COLORS)]
+
+  # Draw all boxes onto image.
+  coordinates_list = []
+  counter_for = 0
+  for box, color in box_to_color_map.items():
+    ymin, xmin, ymax, xmax = box
+    height, width, channels = image.shape
+    ymin = int(ymin*height)
+    ymax = int(ymax*height)
+    xmin = int(xmin*width)
+    xmax = int(xmax*width)
+    coordinates_list.append([ymin, ymax, xmin, xmax, (box_to_score_map[box]*100)])
+    counter_for = counter_for + 1
+
+  return coordinates_list 
 
 class VisualizeSingleFrameDetections(EvalMetricOpsVisualization):
   """Class responsible for single-frame object detection visualizations."""
